00bebf4e7acdf9c4092126b9d594eddcd3be4410
Danil Makushev <reurairin@gmail.com>
2024-02-27 10:03:26 +0300

    initialize client

---
+++ phonogenic-client/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './phonogenic/dashboard/dashboard.component';
+
+export const routes: Routes = [{ path: '', component: DashboardComponent }];
